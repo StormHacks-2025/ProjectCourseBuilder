bb402780e15067426f330e0b247065eabcd3f0bb
Harman Bhogal <bhogalharman7@gmail.com>
2025-10-04 19:30:31 -0700

    got rid of the fake courses in builder as data will be supplied through search

--- frontend/src/pages/Builder.jsx
+++ frontend/src/pages/Builder.jsx
@@ -1,46 +1,29 @@
-import { Slider } from "../component/slider.jsx";
-import { SearchBarBuilder } from "../component/seachBarBuilder.jsx";
+import { useState } from "react";
+import { SearchBarBuilder } from "../component/SearchBarBuilder.jsx";
 import WeeklySchedule from "../component/WeeklySchedule.jsx";
 
 export default function Builder() {
-  const fixedCourses = [
-    { id: "physics", title: "Physics", day: "Tue", startHour: 10, endHour: 12 },
-    { id: "chem", title: "Chemistry", day: "Wed", startHour: 14, endHour: 16 },
-  ];
-
-  const variableCourses = [
-    {
-      title: "Math",
-      variations: [
-        { day: "Mon", startHour: 9, endHour: 11 },
-        { day: "Mon", startHour: 11, endHour: 13 },
-      ],
-    },
-    {
-      title: "English",
-      variations: [
-        { day: "Tue", startHour: 10, endHour: 12 },
-        { day: "Wed", startHour: 14, endHour: 16 },
-      ],
-    },
-  ];
-
+  const [selectedCourses, setSelectedCourses] = useState([]);
+  const [pinnedCourses, setPinnedCourses] = useState([]);
 
   return (
-    <div className="flex flex-col md:flex-row gap-6 p-6">
-      <div className="flex-1">
-        <SearchBarBuilder />
-      </div>
-
-      <div className="flex-2">
-        <WeeklySchedule
-          fixedCourses={fixedCourses}
-          variableCourses={variableCourses}
+    <div className="flex gap-6 p-6">
+      <div className="w-96">
+        <SearchBarBuilder
+          selectedCourses={selectedCourses}
+          setSelectedCourses={setSelectedCourses}
+          pinnedCourses={pinnedCourses}
+          setPinnedCourses={setPinnedCourses}
         />
       </div>
 
-      <div className="hidden md:block">
-        <Slider />
+      <div className="flex-1">
+        <WeeklySchedule
+          pinnedCourses={pinnedCourses}
+          variableCourses={selectedCourses.filter(
+            (c) => !pinnedCourses.includes(c)
+          )}
+        />
       </div>
     </div>
   );
